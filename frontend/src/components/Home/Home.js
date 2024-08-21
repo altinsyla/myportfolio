f23331b4c6e54e6487f2e954b4f997e5cd0af8b4
altinsyla <altinsyla11@gmail.com>
2024-08-21 11:29:48 +0200

    Projects Section
    Already finished the Projects Section, I put one of my project on this section.

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -5,8 +5,19 @@ import About from '../About/About';
 import Skills from '../Skills/Skills';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
+
+  const navigate = useNavigate();
+  const handleScroll = (event, section) => {
+    event.preventDefault();
+    navigate("/");
+    setTimeout(() => {
+      document.getElementById(section).scrollIntoView({ behavior: "smooth" });
+    }, 100); 
+  };
+
   return (
     <div>
       <div className='home-container' id="home">
@@ -18,7 +29,7 @@ function Home() {
         <div className='text-container'>
           <p><span className='highlight'>Hi!</span> I'm <span className='highlight'>Altin</span></p>
           <p>a<span className='highlight'> Web Developer</span></p>
-          <button className='contact-button'>Contact</button>
+          <Link to="/contact" className="contact-button" onClick={(event) => handleScroll(event, "contact")} style={{textDecoration: 'none'}}>Contact</Link>
         </div>
       </div>
       <div id="about">
--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
 import '../Projects/Projects.scss';
 import '../../../src/_styles.scss';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import internnexus from '../../images/internnexus.png';
+import github from '../../images/github.svg';
 
 function Projects() {
   return (
     <div className='projects-container'>
-      <h1 style={{fontFamily:"lato-regular"}}>Projects</h1>
+      <h1 style={{ fontFamily: "lato-regular" }}>Projects</h1>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <div className="project-slide">
+            <div className="image-container">
+              <img src={internnexus} alt="Project Image" className="project-image" />
+              <div className="overlay">
+                <div className="description">
+                InternNexus is a platform that aims to help students finding new Internships in different Kosovo Companies.
+                </div>
+              </div>
+            </div>
+            <h2 className="project-title">InternNexus</h2>
+            <a href="https://github.com/altinsyla/InternNexus" target="_blank" rel="noopener noreferrer" className="github-link">
+              <img src={github} alt='github-icon' className='projects-icons'/>
+              </a>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
