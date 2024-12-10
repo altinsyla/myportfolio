bf5da12e82d10d372e40c67c76344d1dd87e32a6
altinsyla <altinsyla11@gmail.com>
2024-12-10 13:13:56 +0100

    Done for Desktop
    This website is done for desktop view. Now, I have to make it responsive for different screens.

--- frontend/src/components/NavigationBar/NavigationBar.js
+++ frontend/src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/Logo.png";
 import "./NavigationBar.scss";
 
 function NavigationBar() {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleScroll = (event, section) => {
     event.preventDefault();
@@ -14,17 +15,26 @@ function NavigationBar() {
     }, 100); 
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar-container">
       <div className="img-container">
         <img src={logo} className="logo" alt="Logo" />
       </div>
-      <div className="navbartabs">
+      <div className={`navbartabs ${isOpen ? 'open' : ''}`}>
         <Link to="/home" className="navbar-links" onClick={(event) => handleScroll(event, "home")}>Home</Link>
         <Link to="/about" className="navbar-links" onClick={(event) => handleScroll(event, "about")}>About</Link>
         <Link to="/skills" className="navbar-links" onClick={(event) => handleScroll(event, "skills")}>Skills</Link>
         <Link to="/projects" className="navbar-links" onClick={(event) => handleScroll(event, "projects")}>Projects</Link>
         <Link to="/contact" className="navbar-links" onClick={(event) => handleScroll(event, "contact")}>Contact</Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
       </div>
     </div>
   );
